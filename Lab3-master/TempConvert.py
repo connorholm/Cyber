4ae40049a928c8e8e85ef28635235ece6fd75255
Connor Holm <connorjholm@gmail.com>
2020-11-12 11:20:27 -0600

    finished TempConvert.py

--- Lab3-master/TempConvert.py
+++ Lab3-master/TempConvert.py
@@ -6,7 +6,9 @@
 
 def main():
     #Prompt the user for a Fahrenheit temperature
+    Ftemp = int(input("Enter Temp in F: "))
     #Convert that temperature to celsius, rounding to 1 decimal percision
+    Ctemp = (5/9)*(Ftemp-32)
     #Output converted temperature.
-
+    print(round(Ctemp,1))
 main()
