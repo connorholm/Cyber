2706bf7e7bc41d04d902c4b191462f067e81288e
Connor Holm <connorjholm@gmail.com>
2020-11-12 11:36:03 -0600

    Finished Rock Paper Scissors Game

--- Lab3-master/RPS.py
+++ Lab3-master/RPS.py
@@ -1,5 +1,5 @@
 #RPS.py
-#Name:
+#Name: Connor Holm
 #Date:
 #Assignment:
 import random
@@ -10,12 +10,26 @@ def main():
     losses = 0
     #Create a loop that continues as long as the user wants to play.
     #User can play as many games as they wish.
-
-    #Randomly choose the computer between 'R', 'P', or 'S'
-    #Prompt the user for their RPS selection
-    #Determine winner and state what happened to the user
-    #Ask the user if they would like to play again.
-
+    playAgain = True
+    while playAgain:
+        user = input("Select Rock, Paper, or Scissors (R, P, S): ")
+        #Randomly choose the computer between 'R', 'P', or 'S'
+        cpu = random.choice(["R","P","S"])
+        #Prompt the user for their RPS selection
+        print("User: "+user)
+        print("Computer: "+cpu)
+        #Determine winner and state what happened to the user
+        if (cpu == user):
+            ties = 1 + ties
+        elif (cpu == "R" and user == "P") or (cpu == "P" and user == "S") or (cpu == "S" and user == "R"):
+            wins = 1 + wins
+        else:
+            losses = 1 + losses
+        #Ask the user if they would like to play again.
+        if input("Would you like to play again (Yes/No): ") == "Yes":
+            playAgain = True
+        else:
+            playAgain = False
     #In the end, print the stats
     print("Wins \t Ties \t Losses")
     print("---- \t ---- \t ------")
