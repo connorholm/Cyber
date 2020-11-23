619024bede4c6408eee0f960c83a345b0341b35d
Connor Holm <connorjholm@gmail.com>
2020-11-23 11:54:35 -0600

    Finished DiceRoll.py

--- Lab6-master/DiceRoll.py
+++ Lab6-master/DiceRoll.py
@@ -1,17 +1,25 @@
 #DiceRoll.py
-#Name:
+#Name: Connor Holm
 #Date:
 #Assignment:
 import random
 
 def main():
     #Create an empty list with possible roll values
-
+    diceArray = [0,0,0,0,0,0,0,0,0,0,0,0]
     #Create two dice values ranging from 1 - 6 each
-
-    #find the sum total of the two dice
-
+    for i in range(1000):
+        dice1 = random.choice(range(1,7))
+        dice2 = random.choice(range(1,7))
+        #find the sum total of the two dice
+        total = dice1 + dice2
+        diceArray[total-1] = diceArray[total-1] + 1
+    
     #print statictics for dice rolls
-
-
+    index = 0
+    print("Frequency of Dice Rolls: ")
+    for number in diceArray:
+        indexTotal = index+1
+        print(str(indexTotal)+": "+str(number))
+        index = index + 1
 main()
